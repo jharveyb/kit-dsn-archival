52a17c63fb2f6c74f44a136886e4431ca74c85dc
JHB <16675200+jharveyb@users.noreply.github.com>
2026-07-31 13:39:01 -0400

    remove logic for initial project attempt

--- create_zstd_dict.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DICTDIR="$(pwd)/zstd_dict_entries"
-
-# Use all files in zstd_dict_entries as input; optimize for compression
-# level 22.
-zstd --train "$DICTDIR" -r -22 -o "$(pwd)/zstd_dict"
